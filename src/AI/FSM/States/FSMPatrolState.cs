199b192255a8011feb391babe5ecdc08bc5bf21f
Michał Żołdak <michalzoldak97@gmail.com>
2022-07-20 10:47:37 +0200

    visibility calc refractor

--- src/AI/FSM/States/FSMPatrolState.cs
+++ src/AI/FSM/States/FSMPatrolState.cs
@@ -8,15 +8,19 @@ namespace URPMk2
 {
     public class FSMPatrolState : IFSMState
 	{
+        private int sightRangePow;
+        private Teams[] teamsToAttack;
         private Vector3 heading;
         private Transform fTransform;
-        private Collider[] enemyCols;
         private FSMStateManager fManager;
 
         public FSMPatrolState (FSMStateManager fManager)
         {
             this.fManager = fManager;
             fTransform = fManager.transform;
+            sightRangePow = fManager.GetFSMSettings().sightRange * 
+                fManager.GetFSMSettings().sightRange;
+            teamsToAttack = fManager.GetFSMSettings().teamsToAttack;
         }
         public void ToAlertState()
         {
@@ -35,16 +39,20 @@ namespace URPMk2
         private void Look()
         {
             List<ITeamMember> enemiesInRange = TeamMembersManager.GetTeamMembersInRange(
-                    fManager.GetFSMSettings().teamsToAttack,
-                    fManager.transform.position,
-                    fManager.GetFSMSettings().sightRange * fManager.GetFSMSettings().sightRange
+                    teamsToAttack,
+                    fTransform.position,
+                    sightRangePow
                 );
 
             int numEnemies = enemiesInRange.Count;
             for (int i = 0; i < numEnemies; i++)
             {
-                if (CalculateDotProd(enemiesInRange[i].ObjTransform) > 0.0f &&
-                    VisibilityCalculator.IsVisibleSingle(fManager.VisibilityParams, enemiesInRange[i].ObjTransform, enemiesInRange[i].BoundsExtens))
+                if (CalculateDotProd(enemiesInRange[i].ObjTransform) < 0.0f)
+                    continue;
+
+                if (VisibilityCalculator.IsVisibleSingle(fManager.VisibilityParams, heading, enemiesInRange[i].ObjTransform)
+                    || (heading.sqrMagnitude < fManager.VisibilityParams.highResSearchSqrRange &&
+                    VisibilityCalculator.IsVisibleCorners(fManager.VisibilityParams, enemiesInRange[i].ObjTransform, enemiesInRange[i].BoundsExtens)))
                 {
                     Debug.Log("Found  " + enemiesInRange[i].ObjTransform.name);
                 }
--- src/Global/Visibility/VisibilityCalculator.cs
+++ src/Global/Visibility/VisibilityCalculator.cs
@@ -15,55 +15,55 @@ namespace URPMk2
             }
 			return false;
         }
-		public static bool IsVisibleSingle(VisibilityParamContainer origin, Transform target, Vector3 targetBounds)
+		public static bool IsVisibleSingle(VisibilityParamContainer origin, Vector3 checkDir, Transform target)
         {
-			Vector3 oPos = origin.origin.position;
-			Vector3 checkDir = target.position - oPos;
-
 			if (Physics.Raycast(
-				oPos,
-				checkDir, 
-				out RaycastHit hit, 
-				origin.searchRange, 
+				origin.origin.position,
+				checkDir,
+				out RaycastHit hit,
+				origin.searchRange,
 				origin.layersToSearch))
-            {
+			{
 				if (hit.transform == target)
 					return true;
-
-				if (checkDir.sqrMagnitude < origin.highResSearchSqrRange)
-                {
-					Vector3 targetCenter = target.position;
-					Vector3 cornerPos = targetCenter;
-
-					// top center
-					cornerPos.y += targetBounds.y;
-					if (CheckCorner(oPos, (cornerPos - oPos), origin.highResSearchRange, origin.layersToSearch, target))
-						return true;
-
-					// centre front
-					cornerPos.y = targetCenter.y;
-					cornerPos += target.forward * targetBounds.z;
-                    if (CheckCorner(oPos, (cornerPos - oPos), origin.highResSearchRange, origin.layersToSearch, target))
-                        return true;
-
-                    // centre rear
-                    cornerPos = targetCenter - (target.forward * targetBounds.z);
-                    if (CheckCorner(oPos, (cornerPos - oPos), origin.highResSearchRange, origin.layersToSearch, target))
-                        return true;
-
-					// center +x
-					cornerPos = targetCenter + (target.right * targetBounds.x);
-                    if (CheckCorner(oPos, (cornerPos - oPos), origin.highResSearchRange, origin.layersToSearch, target))
-                        return true;
-
-					// center -x
-					cornerPos = targetCenter - (target.right * targetBounds.x);
-                    if (CheckCorner(oPos, (cornerPos - oPos), origin.highResSearchRange, origin.layersToSearch, target))
-                        return true;
-                }
-				return false;
-            }
+				else
+					return false;
+			}
 			return false;
         }
+		public static bool IsVisibleCorners(VisibilityParamContainer origin, Transform target, Vector3 targetBounds)
+        {
+			Vector3 oPos = origin.origin.position;
+			Vector3 targetCenter = target.position;
+			Vector3 cornerPos = targetCenter;
+
+			// top center
+			cornerPos.y += targetBounds.y;
+			if (CheckCorner(oPos, (cornerPos - oPos), origin.highResSearchRange, origin.layersToSearch, target))
+				return true;
+
+			// centre front
+			cornerPos.y = targetCenter.y;
+			cornerPos += target.forward * targetBounds.z;
+			if (CheckCorner(oPos, (cornerPos - oPos), origin.highResSearchRange, origin.layersToSearch, target))
+				return true;
+
+			// centre rear
+			cornerPos = targetCenter - (target.forward * targetBounds.z);
+			if (CheckCorner(oPos, (cornerPos - oPos), origin.highResSearchRange, origin.layersToSearch, target))
+				return true;
+
+			// center +x
+			cornerPos = targetCenter + (target.right * targetBounds.x);
+			if (CheckCorner(oPos, (cornerPos - oPos), origin.highResSearchRange, origin.layersToSearch, target))
+				return true;
+
+			// center -x
+			cornerPos = targetCenter - (target.right * targetBounds.x);
+			if (CheckCorner(oPos, (cornerPos - oPos), origin.highResSearchRange, origin.layersToSearch, target))
+				return true;
+
+			return false;
+		}
 	}
 }
