2c2ed6c9412019cdc7f241c117c9249cc0b2fac9
Michał Żołdak <michalzoldak97@gmail.com>
2022-12-18 23:10:49 +0100

    defender states init

---
+++ src/AI/ML/States/Defender/DefenderCombatState.cs
@@ -0,0 +1,23 @@
+using UnityEngine;
+
+namespace URPMk2
+{
+	public class DefenderCombatState : MonoBehaviour
+	{
+		private void SetInit()
+		{
+			
+		}
+		
+		private void OnEnable()
+		{
+			SetInit();
+			
+		}
+		
+		private void OnDisable()
+		{
+			
+		}
+	}
+}
