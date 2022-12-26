81226ca3e1add7bb7ddafd22d561e51bfff299f4
Michał Żołdak <michalzoldak97@gmail.com>
2022-12-26 01:01:17 +0100

    npc turrent alert allies

--- src/AI/Turrent/NPCTurrent.cs
+++ src/AI/Turrent/NPCTurrent.cs
@@ -1,14 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
 using UnityEngine;
 
 namespace URPMk2
 {
 	public class NPCTurrent : MonoBehaviour
 	{
-		private bool isEnemyLocked;
+		private bool isEnemyLocked, isInformingAllies;
 		private int enemyDetectionCount, enemiesBuffLen;
 		private float checkRate, nextCheck;
 		private Transform target;
-		private NPCMaster npcMaster;
+		private WaitForSeconds waitForNextAlliesInform;
+        private NPCMaster npcMaster;
         private NPCTurrentRotationController rotationController;
 
 		public float GetCheckRate()
@@ -17,13 +20,17 @@ namespace URPMk2
 		}
         private void Awake()
 		{
-			rotationController = GetComponent<NPCTurrentRotationController>();
             npcMaster = GetComponent<NPCMaster>();
 			checkRate = npcMaster.GetFSMSettings().checkRate;
             checkRate = Random.Range(
                 checkRate - npcMaster.GetFSMSettings().checkRateOffset,
                 checkRate + npcMaster.GetFSMSettings().checkRateOffset);
-			enemiesBuffLen = npcMaster.GetFSMSettings().enemiesBufferSize;
+        }
+		private void Start()
+		{
+            enemiesBuffLen = npcMaster.GetFSMSettings().enemiesBufferSize;
+            waitForNextAlliesInform = new WaitForSeconds(npcMaster.GetFSMSettings().informAlliesPeriod);
+            rotationController = GetComponent<NPCTurrentRotationController>();
         }
 		private void ScanForEnemies()
 		{
@@ -54,12 +61,42 @@ namespace URPMk2
 			if (emptyDetections >= enemiesBuffLen)
 				target = null;
         }
-		
-		private void AttackTarget()
+        private IEnumerator ResetInformState()
+        {
+            yield return waitForNextAlliesInform;
+            isInformingAllies = false;
+        }
+        private void AlertAllies()
+        {
+            if (!npcMaster.GetFSMSettings().shouldInformAllies ||
+                isInformingAllies ||
+                !gameObject.activeSelf)
+                return;
+
+            isInformingAllies = true;
+
+            List<ITeamMember> teamMembersInRange = TeamMembersManager.GetTeamMembersInRange(
+				npcMaster.GetFSMSettings().teamID, 
+				transform.position, 
+				npcMaster.GetFSMSettings().informAlliesRangePow);
+
+            int numAllies = teamMembersInRange.Count;
+
+            for (int i = 0; i < numAllies; i++)
+            {
+                if (teamMembersInRange[i].Object.activeSelf &&
+                    teamMembersInRange[i].Object != gameObject)
+                    teamMembersInRange[i].NMaster.CallEventAlertAboutEnemy(target);
+            }
+
+            StartCoroutine(ResetInformState());
+        }
+        private void AttackTarget()
 		{
 			rotationController.StartRotateTowardsTransform(target);
             npcMaster.CallEventAttackTarget(target);
-		}
+			AlertAllies();
+        }
 		private void Update()
 		{
 			if (Time.time < nextCheck)
